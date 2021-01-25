fce83e8ee8d71a9af6071b9340f9187edb66f553
GabrielSilva2y3d <74156876+GabrielSilva2y3d@users.noreply.github.com>
2021-01-25 18:05:26 -0300

    a

---
+++ simuladorDado.py
@@ -0,0 +1,13 @@
+#Simulador de dado
+from random import randint
+
+dado = randint(0,6)
+next = bool(0, 1)
+
+while next == True:
+    if next == True:
+        print(dado)
+        
+    else:
+        print('Você terminou o programa')
+        next = False
